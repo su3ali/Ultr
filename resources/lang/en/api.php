7514da609e7c774202ed47b76c7fa34e11bb0789
BadrAkbik <badrakbik1@gmail.com>
2024-09-08 15:45:39 +0300

    translate

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -43,4 +43,5 @@ return [
     'Your order price didnt reach the minimum value for the coupon !' => 'Your order price didnt reach the minimum value for the coupon !',
     'Invalid Code !' => 'Invalid Code !',
     'This coupon con not be used to any of these services !' => 'This coupon con not be used to any of these services !',
+    'This Time is not available' => 'Sorry, this time is not available'
 ];
