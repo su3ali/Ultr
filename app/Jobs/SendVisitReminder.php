c4e3bde13438317ad2bf0b02babb48e778637d1e
BadrAkbik <badrakbik1@gmail.com>
2024-09-11 11:47:17 +0300

    send visit reminder

---
+++ app/Jobs/SendVisitReminder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Booking;
+use App\Traits\NotificationTrait;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendVisitReminder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, NotificationTrait;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $bookings = Booking::with('customer')->whereDate('date', '=', date('Y-m-d'))
+            ->whereBetween('time', [now()->addMinutes(55)->format('H:i:s'), now()->addMinutes(60)->format('H:i:s')])
+            ->get();
+        $usersFcmTokens = $bookings->pluck('customer.fcm_token')->toArray();
+        if (count($usersFcmTokens) > 0) {
+
+            $notification = [
+                'device_token' => $usersFcmTokens,
+                'title' => 'الترا',
+                'message' => 'اقترب موعد زيارتك',
+                'type' => '',
+                'code' => 2
+            ];
+
+            $this->pushNotification($notification);
+        }
+    }
+}
