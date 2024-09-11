c9f6e06ff1f60e868d96fa3c754dbe4e4008c51b
BadrAkbik <badrakbik1@gmail.com>
2024-09-11 15:18:48 +0300

    scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\SendRateReminder;
 use App\Jobs\SendVisitReminder;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -18,6 +19,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->job(New SendVisitReminder)->everyFiveMinutes();
+        $schedule->job(New SendRateReminder)->everyFiveMinutes();
     }
 
     /**
