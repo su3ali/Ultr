72fafce4b9ba4b4929c743d70dd06235ab77a6f4
BadrAkbik <badrakbik1@gmail.com>
2024-09-12 14:16:20 +0300

    nullOnDelete for user address and car

---
+++ database/migrations/2024_09_12_141408_alter_table_orders_change_user_address_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableOrdersChangeUserAddressId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['user_address_id']);
+            $table->foreign('user_address_id')->references('id')->on('user_addresses')->nullOnDelete()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['user_address_id']);
+            $table->foreign('user_address_id')->references('id')->on('user_addresses')->onDelete('cascade');
+        });
+    }
+}
