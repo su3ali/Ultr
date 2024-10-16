c8d75cc5e994d2849622bdc76c46c6bbd1a86454
waleedcode <waleedflutter@gmail.com>
2024-10-16 15:54:10 +0300

    Feat : Create Seeder For Admin User To have All Regions Access

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             LanguageSeeder::class,
             DaySeeder::class,
             ShiftsSeeder::class,
+            SuperAdminRegionsSeeder::class,
 
         ]);
     }
