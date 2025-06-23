9b5c555e07810f078e0dcfe3a8dc7ee074e42ffd
waleedcode <waleedflutter@gmail.com>
2025-06-23 19:35:58 +0300

    improve ui for busniess orders

--- resources/js/dashboard/business_orders/step1-customer.js
+++ resources/js/dashboard/business_orders/step1-customer.js
@@ -1,89 +1,89 @@
-$(document).ready(function () {
-    $("#saveCustomer").on("click", function () {
-        const phone = $("#phoneInput").val().trim();
-        const fullPhone = "966" + phone;
-        const $btn = $(this);
+// $(document).ready(function () {
+//     $("#saveCustomer").on("click", function () {
+//         const phone = $("#phoneInput").val().trim();
+//         const fullPhone = "966" + phone;
+//         const $btn = $(this);
 
-        // التحقق من صحة الرقم
-        if (!/^5\d{8}$/.test(phone)) {
-            toastr.error(
-                "الرجاء إدخال رقم جوال صحيح مكون من 9 أرقام ويبدأ بـ 5"
-            );
-            return;
-        }
+//         // التحقق من صحة الرقم
+//         if (!/^5\d{8}$/.test(phone)) {
+//             toastr.error(
+//                 "الرجاء إدخال رقم جوال صحيح مكون من 9 أرقام ويبدأ بـ 5"
+//             );
+//             return;
+//         }
 
-        // تعيين القيمة النهائية للرقم داخل الفورم
-        $('input[name="phone"]').val(fullPhone);
+//         // تعيين القيمة النهائية للرقم داخل الفورم
+//         $('input[name="phone"]').val(fullPhone);
 
-        // تعطيل الزر أثناء المعالجة
-        $btn.prop("disabled", true).html(
-            '<span class="spinner-border spinner-border-sm"></span> جاري الحفظ...'
-        );
+//         // تعطيل الزر أثناء المعالجة
+//         $btn.prop("disabled", true).html(
+//             '<span class="spinner-border spinner-border-sm"></span> جاري الحفظ...'
+//         );
 
-        // إرسال البيانات إلى السيرفر
-        $.post(storeCustomerUrl, $("#createCustomerForm").serialize())
-            .done(function (res) {
-                const userId = res.user_id;
-                createdUserId = userId;
-                $("#car_user_id, #order_user_id").val(userId);
+//         // إرسال البيانات إلى السيرفر
+//         $.post(storeCustomerUrl, $("#createCustomerForm").serialize())
+//             .done(function (res) {
+//                 const userId = res.user_id;
+//                 createdUserId = userId;
+//                 $("#car_user_id, #order_user_id").val(userId);
 
-                // عرض السيارات إن وجدت
-                if (res.cars && res.cars.length > 0) {
-                    debugger;
-                    renderUserCars(res.cars);
-                } else {
-                    showCreateCarOption();
-                }
+//                 // عرض السيارات إن وجدت
+//                 if (res.cars && res.cars.length > 0) {
+//                     debugger;
+//                     renderUserCars(res.cars);
+//                 } else {
+//                     showCreateCarOption();
+//                 }
 
-                // الانتقال للخطوة الثانية
-                $("#stepperTabs .nav-link").eq(1).tab("show");
-                toastr.success(res.message || "تم حفظ العميل بنجاح");
-            })
-            .fail(function (xhr) {
-                if (xhr.status === 422 && xhr.responseJSON?.errors) {
-                    const errors = xhr.responseJSON.errors;
-                    const allMessages = Object.values(errors).map(
-                        (msg) => msg[0]
-                    );
-                    toastr.error(allMessages.join("<br>"));
-                } else {
-                    console.error(xhr.responseText);
-                    toastr.error("حدث خطأ أثناء حفظ العميل");
-                }
-            })
-            .always(function () {
-                $btn.prop("disabled", false).html("التالي");
-            });
-    });
+//                 // الانتقال للخطوة الثانية
+//                 $("#stepperTabs .nav-link").eq(1).tab("show");
+//                 toastr.success(res.message || "تم حفظ العميل بنجاح");
+//             })
+//             .fail(function (xhr) {
+//                 if (xhr.status === 422 && xhr.responseJSON?.errors) {
+//                     const errors = xhr.responseJSON.errors;
+//                     const allMessages = Object.values(errors).map(
+//                         (msg) => msg[0]
+//                     );
+//                     toastr.error(allMessages.join("<br>"));
+//                 } else {
+//                     console.error(xhr.responseText);
+//                     toastr.error("حدث خطأ أثناء حفظ العميل");
+//                 }
+//             })
+//             .always(function () {
+//                 $btn.prop("disabled", false).html("التالي");
+//             });
+//     });
 
-    //  وظائف مساعدة
+//     //  وظائف مساعدة
 
-    function renderUserCars(cars) {
-        let html = `<label class="mb-2 font-weight-bold d-block">اختر سيارة:</label>`;
-        cars.forEach((car) => {
-            html += `
-                <div class="form-check mb-2 car-radio-wrapper">
-                    <input class="form-check-input" type="radio" name="car_id" id="car_${car.id}" value="${car.id}">
-                    <label class="form-check-label" for="car_${car.id}">${car.plate_number}</label>
-                </div>`;
-        });
+//     function renderUserCars(cars) {
+//         let html = `<label class="mb-2 font-weight-bold d-block">اختر سيارة:</label>`;
+//         cars.forEach((car) => {
+//             html += `
+//                 <div class="form-check mb-2 car-radio-wrapper">
+//                     <input class="form-check-input" type="radio" name="car_id" id="car_${car.id}" value="${car.id}">
+//                     <label class="form-check-label" for="car_${car.id}">${car.plate_number}</label>
+//                 </div>`;
+//         });
 
-        $("#existingCarsContainer").html(html).show();
-        $("#createCarSection").hide();
+//         $("#existingCarsContainer").html(html).show();
+//         $("#createCarSection").hide();
 
-        // إضافة تغيير اللون عند التحديد
-        $('input[name="car_id"]').on("change", function () {
-            $(".car-radio-wrapper").removeClass("bg-selected");
-            $(this).closest(".car-radio-wrapper").addClass("bg-selected");
-        });
-    }
+//         // إضافة تغيير اللون عند التحديد
+//         $('input[name="car_id"]').on("change", function () {
+//             $(".car-radio-wrapper").removeClass("bg-selected");
+//             $(this).closest(".car-radio-wrapper").addClass("bg-selected");
+//         });
+//     }
 
-    function showCreateCarOption() {
-        $("#existingCarsContainer")
-            .html(
-                '<div class="alert alert-warning">لا توجد سيارات مسجلة لهذا العميل.</div>'
-            )
-            .show();
-        $("#createCarSection").show();
-    }
-});
+//     function showCreateCarOption() {
+//         $("#existingCarsContainer")
+//             .html(
+//                 '<div class="alert alert-warning">لا توجد سيارات مسجلة لهذا العميل.</div>'
+//             )
+//             .show();
+//         $("#createCarSection").show();
+//     }
+// });
--- resources/views/dashboard/business_orders/partials/customer.blade.php
+++ resources/views/dashboard/business_orders/partials/customer.blade.php
@@ -1,55 +1,62 @@
+
+
 <div class="tab-pane fade show active" id="step1">
     <form id="createCustomerForm">
         @csrf
 
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label class="font-weight-medium">{{ __('dash.first name') }}</label>
-                <input type="text" name="first_name" class="form-control" placeholder="{{ __('dash.first name') }}"
-                    required>
+        {{-- Search phone --}}
+        <div id="phoneSearchSection" class="form-group">
+            <label class="font-weight-medium">{{ __('dash.phone') }}</label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text bg-light border-right-0">+966</span>
+                </div>
+                <input type="text" id="phoneInput" class="form-control border-left-0" placeholder="5xxxxxxxx"
+                    maxlength="9">
+                <div class="input-group-append">
+                    <button type="button" class="btn btn-outline-primary" id="checkPhoneBtn">بحث</button>
+                </div>
             </div>
-
-            <div class="form-group col-md-6">
-                <label class="font-weight-medium">{{ __('dash.last name') }}</label>
-                <input type="text" name="last_name" class="form-control" placeholder="{{ __('dash.last name') }}"
-                    required>
-            </div>
+            <small class="form-text text-muted">رقم الجوال يبدأ بـ 5 ويتكون من 9 أرقام</small>
         </div>
 
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label class="font-weight-medium">{{ __('dash.phone') }}</label>
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text bg-light border-right-0">+966</span>
-                    </div>
-                    <input type="text" name="phone" id="phoneInput" class="form-control border-left-0"
-                        placeholder="5xxxxxxxx" maxlength="9">
+
+
+        {{-- Additional customer fields, hidden initially --}}
+        <div id="customerFields" style="display: none;">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label class="font-weight-medium">{{ __('dash.first name') }}</label>
+                    <input type="text" name="first_name" class="form-control" placeholder="{{ __('dash.first name') }}"
+                        required>
                 </div>
-                <small class="form-text text-muted">رقم الجوال يبدأ بـ 5 ويتكون من 9 أرقام</small>
+
+                <div class="form-group col-md-6">
+                    <label class="font-weight-medium">{{ __('dash.last name') }}</label>
+                    <input type="text" name="last_name" class="form-control" placeholder="{{ __('dash.last name') }}"
+                        required>
+                </div>
             </div>
 
-            <div class="form-group col-md-6">
-                <label class="font-weight-medium">
-                    {{ __('dash.city') }} <span class="text-danger">*</span>
-                </label>
-                <select name="city_id" class="form-control select2" required>
-                    <option value="">{{ __('dash.choose') }}</option>
-                    @foreach($cities as $id => $name)
-                    <option value="{{ $id }}">{{ $name }}</option>
-                    @endforeach
-                </select>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label class="font-weight-medium">{{ __('dash.city') }} <span class="text-danger">*</span></label>
+                    <select name="city_id" class="form-control select2" required>
+                        <option value="">{{ __('dash.choose') }}</option>
+                        @foreach($cities as $id => $name)
+                        <option value="{{ $id }}">{{ $name }}</option>
+                        @endforeach
+                    </select>
+                </div>
             </div>
         </div>
 
         <input type="hidden" id="car_user_id" name="car_user_id">
         <input type="hidden" id="order_user_id" name="order_user_id">
+        <input type="hidden" name="phone">
 
-
-
-        {{-- زر التالي --}}
         <div class="text-right mt-4">
-            <button type="button" id="saveCustomer" class="btn btn-success">
+            <button type="button" id="saveCustomer" class="btn btn-success" style="display: none;">
                 {{ __('dash.next') }} <i class="fas fa-arrow-left ml-1"></i>
             </button>
         </div>
